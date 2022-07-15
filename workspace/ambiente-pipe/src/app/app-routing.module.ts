62bacd514df74cd55d8aec6be4ded6b8977bef8e
Fernando <fernando.luiz92@hotmail.com>
2022-07-15 20:38:59 -0300

    Desenvolvimento do componente pipes-example

--- workspace/ambiente-pipe/src/app/app-routing.module.ts
+++ workspace/ambiente-pipe/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PipesExampleComponent } from './pages/pipes-example/pipes-example.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'pipes', component: PipesExampleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- workspace/ambiente-pipe/src/app/app.module.ts
+++ workspace/ambiente-pipe/src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PipesExampleComponent } from './pages/pipes-example/pipes-example.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    PipesExampleComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatGridListModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
